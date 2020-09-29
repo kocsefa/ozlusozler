664bd8dc10056771ec7ffe2ca752e22d50641f72
Sefa Koç <koc.sefa@hotmail.com>
2020-09-29 16:20:11 +0300

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  async function fetchQuotation() {
+    let number = Math.floor(Math.random() * 100);
+    let res = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${number}`
+    );
+    if (res.ok) {
+      return res.json();
+    }
+  }
+
+  $: promise = fetchQuotation();
+</script>
+
+<div class="flex h-full w-full items-center justify-center">
+  <div class="w-9/12 p-4 text-center rounded bg-white bg-opacity-50 relative">
+    {#await promise}
+      <span class="text-3xl">Yükleniyor...</span>
+    {:then post}
+      <div class="text-3xl mb-8">{post.body}</div>
+      <div class="absolute right-0 bottom-0 m-4">{post.title}</div>
+    {/await}
+  </div>
+</div>
